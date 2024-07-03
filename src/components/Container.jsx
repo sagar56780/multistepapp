3f012f8fac47c8dcfa49a0ea31ebe6a9735b7622
sagar Kumar <sagat56780@gmail.com>
2024-07-03 14:31:22 +0530

    responisve for mobile

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,16 +1,43 @@
 
+import { useEffect, useState } from "react";
 import Sidebar from "./Sidebar";
 import { Outlet, useNavigate } from "react-router-dom";
 
 const Container = () => {
   let nav = useNavigate();
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [showSidebar, setShowSidebar] = useState(window.innerWidth <= 500);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    // Add event listener for window resize
+    window.addEventListener('resize', handleResize);
+
+    // Clean up the event listener on component unmount
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    // Update showSidebar based on windowWidth
+    setShowSidebar(windowWidth >= 500);
+  }, [windowWidth]);
+
+ 
 
 
 
-  return (
+  return ( 
     <div className="container">
+      {!showSidebar&&<Sidebar count={nav}></Sidebar>}
       <section>
-        <Sidebar count={nav}></Sidebar>
+       {showSidebar&&<Sidebar count={nav}></Sidebar>
+      
+        }
         <div className="info-container">
           <Outlet></Outlet>
         </div>
